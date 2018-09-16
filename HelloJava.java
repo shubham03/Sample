09fcb69275bed872b99280917975689e04430e7c
Shubham Gupta <Shubham.Gupta@mpowersoft.in>
2018-09-16 19:44:26 +0530

    Adding Hello Java Files

---
+++ HelloJava.java
@@ -0,0 +1,7 @@
+class HelloJava
+{
+	public static void mian(String... sd)
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ HelloJava2.java
@@ -0,0 +1,7 @@
+class HelloJava2
+{
+	public static void mian(String... sd)
+	{
+		System.out.println("Hello World");
+	}
+}
